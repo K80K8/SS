a71d959b0f6d5ab48dae5664c7a4ed7ad8426404
Kate <140380673+K80K8@users.noreply.github.com>
2025-10-16 13:50:40 +0800

    Delete report_generator/run_report_generator.bat

--- report_generator/run_report_generator.bat
+++
@@ -1,3 +0,0 @@
-    Set WshShell = CreateObject("WScript.Shell")
-    WshShell.Run "powershell.exe -NoProfile -WindowStyle Hidden -File ""report_generator.ps1""", 0, False
-    Set WshShell = Nothing
